d36c6853e732d8715935605ada61b42773e4aa90
Matheus Munhoz <69037408+MrSchrodingers@users.noreply.github.com>
2023-11-18 12:46:17 -0300

    Add files via upload

---
+++ codigos.py
@@ -0,0 +1,18 @@
+codigos_sgs = { "1207" :    "PIB - Produto interno bruto em R$ correntes",
+                "432" :     "SELIC - Meta Selic definida pelo Copom",
+                "433" :     "IPCA - Índice nacional de preços ao consumidor-amplo",
+                "24369" :   "PNADC - Taxa de desocupação",
+                "2732" :    "Balança comercial",
+                "12" :      "CDI - Taxa de juros",
+                "189" :     "IGP-M - Índice geral de preços do mercado",
+                "1":        "Taxa de câmbio - Dólar americano",
+                "2054":     "Dívida Líquida do Setor Público - Governo Federal",
+                "2055":     "Dívida Líquida do Setor Público - Banco Central",
+                "4536":     "Dívida líquida do governo geral (% PIB)",
+                "4571":     "Dívida líquida do governo geral - Saldos em US$ milhões",
+                "7811" :    "TR - Taxa Referencial",
+                "192" :     "INCC - Índice nacional de custo da construção (Inflação)",
+                "188" :     "INPC - Índice nacional de preços ao consumidor",
+                "1619":     "Salário Mínimo"
+
+    }
